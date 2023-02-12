028d4cacfe6f8894a0ae75695cde7f0f99e705b1
Richard Schneider <richard@schneiderbox.net>
2023-02-12 08:02:17 -0600

    Switch UI to use React

---
+++ src/ui/modules/Grid.js
@@ -0,0 +1,91 @@
+import { e } from "./shortcuts.js";
+
+import { Player, Type } from "./enum.js";
+import { HEX_SIZE } from "./hex.js";
+import { Piece } from "./piece.js";
+
+const MAX_PIECES = 22;
+
+function draw(state, canvas) {
+  console.log("Drawing " + state);
+  const grid = [];
+
+  let minX = MAX_PIECES;
+  let maxX = 0;
+  let minY = MAX_PIECES;
+  let maxY = 0;
+
+  // Convert state (string) into Pieces in grid
+  for (let i = 0; i + 2 < state.length; i += 3) {
+    let piece = Piece.fromChar(state[i]);
+    let q = state[i + 1].charCodeAt(0) - "a".charCodeAt(0);
+    let r = state[i + 2].charCodeAt(0) - "a".charCodeAt(0);
+
+    let x = q;
+    let y = 2 * r + q;
+
+    if (grid[x] === undefined) {
+      grid[x] = [];
+    }
+
+    if (!grid[x][y]) {
+      grid[x][y] = piece;
+    } else {
+      let p = grid[x][y];
+      while (p.on_top) {
+        p = p.on_top;
+      }
+      p.on_top = piece;
+    }
+
+    minX = Math.min(minX, x);
+    maxX = Math.max(maxX, x);
+    minY = Math.min(minY, y);
+    maxY = Math.max(maxY, y);
+  }
+
+  let gridWidth = maxX - minX + 1;
+  let gridHeight = maxY - minY + 1;
+
+  canvas.width = 2 * HEX_SIZE + 1.5 * HEX_SIZE * (gridWidth - 1);
+  canvas.height =
+    HEX_SIZE * Math.sqrt(3) + 0.5 * HEX_SIZE * Math.sqrt(3) * (gridHeight - 1);
+
+  const ctx = canvas.getContext("2d");
+
+  for (let x = 0; x < MAX_PIECES; x += 2) {
+    for (let y = 0; y < MAX_PIECES; y += 2) {
+      ctx.save();
+
+      ctx.translate(
+        HEX_SIZE + (x - minX) * 1.5 * HEX_SIZE,
+        0.5 * HEX_SIZE * Math.sqrt(3) +
+          (y - minY) * 0.5 * HEX_SIZE * Math.sqrt(3)
+      );
+
+      if (grid[x] && grid[x][y]) {
+        let piece = grid[x][y];
+        piece.draw(ctx);
+      }
+
+      ctx.translate(1.5 * HEX_SIZE, 0.5 * HEX_SIZE * Math.sqrt(3));
+
+      if (grid[x + 1] && grid[x + 1][y + 1]) {
+        let piece = grid[x + 1][y + 1];
+        piece.draw(ctx);
+      }
+
+      ctx.restore();
+    }
+  }
+}
+
+export default function Grid({ state }) {
+  const canvasRef = React.useRef(null);
+
+  React.useEffect(() => {
+    draw(state, canvasRef.current);
+  }, [state]);
+
+  return e("canvas", { ref: canvasRef });
+}
---
+++ src/ui/modules/piece.js
@@ -0,0 +1,58 @@
+import { Colors } from "./colors.js";
+import { Player, Type } from "./enum.js";
+import { HEX } from "./hex.js";
+
+const GAP_SCALE = 0.95;
+const TYPE_SCALE = 0.75;
+
+// Applied with GAP_SCALE but not TYPE_SCALE
+const ON_TOP_SCALE = TYPE_SCALE;
+const ON_TOP_ROTATION = Math.PI / 12;
+
+export class Piece {
+  constructor(player, type) {
+    this.player = player;
+    this.type = type;
+
+    this.on_top = null;
+  }
+
+  static fromChar(c) {
+    let player = c === c.toUpperCase() ? Player.P1 : Player.P2;
+
+    let type = {
+      a: Type.Ant,
+      b: Type.Beetle,
+      g: Type.Grasshopper,
+      q: Type.QueenBee,
+      s: Type.Spider,
+    }[c.toLowerCase()];
+
+    return new Piece(player, type);
+  }
+
+  draw(ctx) {
+    ctx.save();
+    ctx.scale(GAP_SCALE, GAP_SCALE);
+
+    ctx.fillStyle = Colors[this.player];
+    ctx.fill(HEX);
+
+    ctx.scale(TYPE_SCALE, TYPE_SCALE);
+
+    ctx.fillStyle = Colors[this.type];
+    ctx.fill(HEX);
+
+    ctx.restore();
+
+    if (this.on_top) {
+      ctx.save();
+
+      ctx.scale(ON_TOP_SCALE, ON_TOP_SCALE);
+      ctx.rotate(ON_TOP_ROTATION);
+      this.on_top.render(ctx, true);
+
+      ctx.restore();
+    }
+  }
+}
