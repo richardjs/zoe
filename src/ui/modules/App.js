b54bd6d4b102d643475dd086bfc434516ab1dd94
Richard Schneider <richard@schneiderbox.net>
2023-02-13 22:11:03 -0600

    Fetch actions for a state

--- src/ui/modules/App.js
+++ src/ui/modules/App.js
@@ -3,20 +3,36 @@ import { e } from "./shortcuts.js";
 
 export default function App() {
   const [state, setState] = React.useState(location.hash.slice(1));
+  const [actions, setActions] = React.useState(null);
 
   React.useEffect(() => {
-    console.log("App effect");
-
     function handleHashChange() {
-      console.log('hash change');
       setState(location.hash.slice(1));
     }
     window.addEventListener("hashchange", handleHashChange);
 
     return () => {
-      window.removeEventListener('hashchange', handleHashChange);
+      window.removeEventListener("hashchange", handleHashChange);
     };
   }, []);
+
+  React.useEffect(() => {
+    let ignore = false;
+
+    function fetchActions() {
+      fetch("/state/" + state + "/actions")
+        .then((response) => response.json())
+        .then((json) => {
+          if (ignore) return;
+          setActions(json.actions);
+        });
+    }
+    fetchActions();
+
+    return () => {
+      ignore = true;
+    };
+  }, [state]);
 
   return e(Grid, { state: state });
 }
