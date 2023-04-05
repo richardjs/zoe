86437a3962d8924a6ecf8900fb3231d4d0bee278
Richard Schneider <richard@schneiderbox.net>
2023-04-05 08:38:05 -0500

    Restructure simulation

--- src/simulate.c
+++ src/simulate.c
@@ -21,9 +21,6 @@ float State_simulate(struct State* state,
 
     enum Player original_turn = state->turn;
 
-    //int last_queen_move_count[NUM_PLAYERS] = {-1, -1};
-    //int last_movable_ants[NUM_PLAYERS] = {-1, -1};
-
     int depth = 0;
     while (state->result == NO_RESULT) {
         if (state->winning_action) {
@@ -31,118 +28,48 @@ float State_simulate(struct State* state,
             continue;
         }
 
-        int moveable_ants = state->hands[state->turn][ANT];
-        for (int i = 0; i < state->piece_count[state->turn]; i++) {
-            struct Piece *piece = &state->pieces[state->turn][i];
-            if (piece->type != ANT) continue;
-
-            if (state->piece_move_count[i]) {
-                moveable_ants++;
-            }
-        }
-        if (moveable_ants == 0) {
-            return -1.0;
-        }
-
-        if (state->cut_point_count[!original_turn] - state->cut_point_count[original_turn] >= options->cut_point_diff_terminate) {
-            stats->cut_point_terminations++;
-            return 1.0;
-        }
-        if (state->cut_point_count[!original_turn] - state->cut_point_count[original_turn] <= -options->cut_point_diff_terminate) {
-            stats->cut_point_terminations++;
-            return -1.0;
-        }
-
         if (depth++ > options->max_sim_depth) {
             stats->depth_outs++;
             return 0.0;
         }
 
+        // select_action:
+
         struct Action* action = NULL;
-        while (1) {
-            if (options->queen_sidestep_bias && state->queen_move_count) {
-                struct Action* actions[MAX_QUEEN_MOVES];
-                int action_count = 0;
-                for (int i = 0; i < state->queen_move_count; i++) {
-                    struct Action* action = state->queen_moves[i];
-                    if (State_is_queen_sidestep(state, action)) {
-                        actions[action_count++] = action;
-                    }
-                }
 
-                if (action_count
-                    && (rand() / (float)RAND_MAX) < options->queen_sidestep_bias) {
-                    action = actions[rand() % action_count];
-                    goto action_chosen;
+        if (options->queen_sidestep_bias && state->queen_move_count) {
+            struct Action* actions[MAX_QUEEN_MOVES];
+            int action_count = 0;
+            for (int i = 0; i < state->queen_move_count; i++) {
+                struct Action* action = state->queen_moves[i];
+                if (State_is_queen_sidestep(state, action)) {
+                    actions[action_count++] = action;
                 }
             }
 
-            if (state->queen_adjacent_action_count
-                && (rand() / (float)RAND_MAX) < options->queen_adjacent_action_bias) {
-                action = state->queen_adjacent_actions[rand() % state->queen_adjacent_action_count];
-                goto action_chosen;
+            if (action_count
+                && (rand() / (float)RAND_MAX) < options->queen_sidestep_bias) {
+                action = actions[rand() % action_count];
+                goto action_selected;
             }
-
-            if (state->beetle_move_count
-                && (rand() / (float)RAND_MAX) < options->beetle_move_bias) {
-                action = state->beetle_moves[rand() % state->beetle_move_count];
-                goto action_chosen;
-            }
-
-            action = &state->actions[rand() % state->action_count];
-
-            struct State next;
-            State_copy(state, &next);
-            State_act(&next, action);
-
-            if (next.cut_point_count[!state->turn] - next.cut_point_count[state->turn]
-                    <= state->cut_point_count[!state->turn] - state->cut_point_count[state->turn]
-                && rand() / (float)RAND_MAX < .9) {
-                continue;
-            }
-
-        action_chosen:
-
-            State_act(state, action);
-            break;
         }
 
-        //if (state->queens[state->turn]) {
-        //    last_queen_move_count[state->turn] = state->queen_move_count;
-        //}
+        if (state->queen_adjacent_action_count
+            && (rand() / (float)RAND_MAX) < options->queen_adjacent_action_bias) {
+            action = state->queen_adjacent_actions[rand() % state->queen_adjacent_action_count];
+            goto action_selected;
+        }
 
-        //pick_action:
-        //bool queen_adjacent_action = false;
-        //if (state->queen_adjacent_action_count
-        //    && (rand() / (float)RAND_MAX) < options->queen_adjacent_action_bias) {
-        //    action = state->queen_adjacent_actions[rand() % state->queen_adjacent_action_count];
-        //    queen_adjacent_action = true;
-        //} else {
-        //    action = &state->actions[rand() % state->action_count];
-        //}
+        if (state->beetle_move_count
+            && (rand() / (float)RAND_MAX) < options->beetle_move_bias) {
+            action = state->beetle_moves[rand() % state->beetle_move_count];
+            goto action_selected;
+        }
 
-        //struct State next;
-        //State_copy(state, &next);
-        //State_act(&next, action);
+        action = &state->actions[rand() % state->action_count];
 
-        //if (state->winning_action
-        //    && rand() / (float)RAND_MAX < .95) {
-        //    goto pick_action;
-        //}
-
-        //if (next.cut_point_count[!state->turn] < state->cut_point_count[!state->turn]
-        //    && !queen_adjacent_action
-        //    && rand() / (float)RAND_MAX < .5) {
-        //    goto pick_action;
-        //}
-
-        //if (last_queen_move_count[next.turn] >= 0
-        //    && next.queen_move_count > last_queen_move_count[next.turn]
-        //    && rand() / (float)RAND_MAX < .9) {
-        //    goto pick_action;
-        //}
-
-        // State_act(state, action);
+    action_selected:
+        State_act(state, action);
     }
 
     stats->mean_sim_depth += (depth - stats->mean_sim_depth) / stats->simulations;
