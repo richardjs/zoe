999b80db0150db5f911118650075a25901e52f63
Richard Schneider <richard@schneiderbox.net>
2023-04-09 04:07:28 -0500

    Add missing examine files

---
+++ src/examine.c
@@ -0,0 +1,7 @@
+#include "examine.h"
+#include "simulate.h"
+#include "state.h"
+
+void State_examine(const struct State* state)
+{
+}
---
+++ src/examine.h
@@ -0,0 +1,8 @@
+#ifndef EXAMINE_H
+#define EXAMINE_H
+
+#include "state.h"
+
+void State_examine(const struct State* state);
+
+#endif
