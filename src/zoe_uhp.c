c47e271e226e37ffa860319a2bca9ba4131ba3a2
Richard Schneider <richard@schneiderbox.net>
2023-05-15 21:03:30 -0500

    Initialize coords map in UHP iterface (fixes #15)

--- src/zoe_uhp.c
+++ src/zoe_uhp.c
@@ -1,7 +1,14 @@
+#include "coords.h"
 #include "uhp.h"
 
 int main()
 {
+    // TODO don't use seed while we're hammering out UHP bugs
+    // time_t seed = time(NULL);
+    // srand(seed);
+
+    init_coords();
+
     uhp_loop();
 
     return 0;
